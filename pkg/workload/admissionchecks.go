dfcf125c28b492ed6e12b3ea9796f1810f24cd2d
Dawid Pastuszka <dpastuszka@google.com>
2026-03-16 12:49:37 +0100

    Reorganized the implemetnation to ensure scheduler conisders admissions without flavor same as checks without admission in core workload controller. (#4)

--- pkg/workload/admissionchecks.go
+++ pkg/workload/admissionchecks.go
@@ -160,13 +160,7 @@ func HasAllChecksReady(wl *kueue.Workload) bool {
 
 // HasAllRequiredChecks returns true if all the relevant checks are present in the workload.
 // (They don't have to be in the Ready state; for that, see HasAllChecksReady).
-func HasAllRequiredChecks(wl *kueue.Workload, allChecks map[kueue.AdmissionCheckReference]sets.Set[kueue.ResourceFlavorReference]) bool {
-	admissionFlavors := admissionFlavors(wl.Status.Admission)
-	if admissionFlavors.Len() == 0 {
-		return true
-	}
-
-	mustHaveChecks := filterChecksForFlavors(allChecks, admissionFlavors)
+func HasAllRequiredChecks(wl *kueue.Workload, mustHaveChecks sets.Set[kueue.AdmissionCheckReference]) bool {
 	if mustHaveChecks.Len() == 0 {
 		return true
 	}
